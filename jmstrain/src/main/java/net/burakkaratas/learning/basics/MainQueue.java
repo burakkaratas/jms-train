36466205dc7f5d5d3d1fbd051bbab6a980af425e
burak <bur.karatas@gmail.com>
2019-12-28 15:46:16 +0300

    jms 1.x

---
+++ jmstrain/src/main/java/net/burakkaratas/learning/basics/MainQueue.java
@@ -0,0 +1,47 @@
+package net.burakkaratas.learning.basics;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class MainQueue {
+
+  public static void main(String[] args) {
+
+    InitialContext initialContext = null;
+
+    try {
+      initialContext = new InitialContext();
+
+      ConnectionFactory connectionFactory = (ConnectionFactory) initialContext
+          .lookup("ConnectionFactory");
+      Queue queue = (Queue) initialContext.lookup("queue/appQueue");
+
+      Connection connection = connectionFactory.createConnection();
+      Session session = connection.createSession();
+
+      MessageProducer producer = session.createProducer(queue);
+      TextMessage textMessage = session.createTextMessage("Selam");
+      producer.send(textMessage);
+
+      MessageConsumer consumer = session.createConsumer(queue);
+      connection.start();
+      TextMessage receive = (TextMessage) consumer.receive();
+      System.out.println(receive.getText());
+
+    } catch (NamingException e) {
+      e.printStackTrace();
+    } catch (JMSException e) {
+      e.printStackTrace();
+    }
+  }
+
+}
